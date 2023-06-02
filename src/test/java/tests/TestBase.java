cb3f7fcedd437f0c912d25c61f12e1e13304ba66
Islam-Kaziev <kaziev19691@gmail.com>
2023-06-02 19:34:06 +0300

    Used owner library

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,14 +16,18 @@ import java.util.Map;
 
 public class TestBase {
         RegistrationPage registrationPage = new RegistrationPage();
+    static WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-        Configuration.remote =  System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.browserSize = webConfig.getBrowserSize();
+        Configuration.browser = webConfig.getBrowser();
+        Configuration.browserVersion = webConfig.getBrowserVersion();
+        Configuration.baseUrl = webConfig.getBaseUrl();
+        if (webConfig.getRemoteUrl() != null){
+            Configuration.remote = webConfig.getRemoteUrl();
+        }
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
